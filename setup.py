cf19167c3001975fa67c93842aae83cdc0ab90de
TJ20201 <86313252+TJ20201@users.noreply.github.com>
2022-02-23 09:49:09 +0000

    Import setup, pyproject and TextUtil

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='tjpythonutilities',
+    version='1.0.0',    
+    description='TJ\'s Utilities for Python',
+    url='https://github.com/TJ20201/tjpythonutilities',
+    author='TJ20201',
+    packages=['utilities'],
+    classifiers=[
+        'Programming Language :: Python :: 3.10',
+    ],
+)
